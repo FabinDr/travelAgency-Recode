f87b438c08ca0f6a63aa13ca2253de17f671996b
= <=>
2025-02-02 12:44:34 -0300

    primeiro commit

---
+++ frontend/menu.js
@@ -0,0 +1,8 @@
+// função para a barra do menu
+
+window.addEventListener("scroll", function () {
+    // pega o header
+    let header = document.querySelector('#header')
+    //adciona uma classe ao header usando js
+    header.classList.toggle('scroll-class', window.scrollY > 500)
+})
